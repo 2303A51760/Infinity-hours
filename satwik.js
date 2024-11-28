1196d77deafa89d88d05828f18b080202b9649c8
2303A51760 <2303A51760@sru.edu.in>
2024-11-28 14:52:07 +0530

    Add files via upload

---
+++ satwik.js
@@ -0,0 +1,6 @@
+// Script for handling button clicks and transitions
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Button clicked! You can add cart functionality here.');
+    });
+});
